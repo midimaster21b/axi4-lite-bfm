2e766789d3b143bef2475d697708c3f3c93096ff
Joshua Edgcombe <joshedgcombe@gmail.com>
2025-08-22 00:15:40 -0500

    Move to constants rather than $bits to resolve elaboration time issues

--- src/rtl/axi4_lite_master_bfm.sv
+++ src/rtl/axi4_lite_master_bfm.sv
@@ -1,72 +1,135 @@
 module axi4_lite_master_bfm(conn);
    axi4_lite_if conn;
 
+   ////////////////////////////////////////////////////////////////////////////
+   // Bit widths
+   ////////////////////////////////////////////////////////////////////////////
+   localparam NUM_ADDR_BITS  = conn.NUM_ADDR_BITS;
+   localparam NUM_DATA_BITS  = conn.NUM_DATA_BITS;
+   localparam NUM_STRB_BITS  = conn.NUM_STRB_BITS;
+   localparam NUM_RESP_BITS  = conn.NUM_RESP_BITS;
+   localparam NUM_PROT_BITS  = conn.NUM_PROT_BITS;
+
+   // Write address
+   localparam NUM_AWADDR_BITS = NUM_ADDR_BITS;
+   localparam NUM_AWPROT_BITS = NUM_PROT_BITS;
+
+   // Write data channel
+   localparam NUM_WDATA_BITS  = NUM_DATA_BITS;
+   localparam NUM_WSTRB_BITS  = NUM_STRB_BITS;
+
+   // Write response channel
+   localparam NUM_BRESP_BITS  = NUM_RESP_BITS;
+
+   // Read address channel
+   localparam NUM_ARADDR_BITS = NUM_ADDR_BITS;
+   localparam NUM_ARPROT_BITS = NUM_PROT_BITS;
+
+   // Read data channel
+   localparam NUM_RDATA_BITS  = NUM_DATA_BITS;
+   localparam NUM_RRESP_BITS  = NUM_RESP_BITS;
+
+
+
+   ////////////////////////////////////////////////////////////////////////////
+   // Offsets
+   ////////////////////////////////////////////////////////////////////////////
+   // Write address
+   localparam AWADDR_OFFSET = 0;
+   localparam AWPROT_OFFSET = AWADDR_OFFSET + NUM_ADDR_BITS;
+
+   // Write data
+   localparam WDATA_OFFSET  = 0;
+   localparam WSTRB_OFFSET  = WDATA_OFFSET + NUM_DATA_BITS;
+
+   // Write response
+   localparam BRESP_OFFSET  = 0;
+
+   // Read address
+   localparam ARADDR_OFFSET = 0;
+   localparam ARPROT_OFFSET = ARADDR_OFFSET + NUM_ADDR_BITS;
+
+   // Read data
+   localparam RDATA_OFFSET  = 0;
+   localparam RRESP_OFFSET  = RDATA_OFFSET + NUM_DATA_BITS;
+
+
+   ////////////////////////////////////////////////////////////////////////////
+   // Channel Structs
+   ////////////////////////////////////////////////////////////////////////////
    typedef struct {
       // Write address channel
-      logic                          awvalid;
-      logic                          awready;
-      logic [$bits(conn.awaddr)-1:0] awaddr;
-      logic [$bits(conn.awprot)-1:0] awprot;
+      logic                     awvalid;
+      logic                     awready;
+      logic [NUM_ADDR_BITS-1:0]	awaddr;
+      logic [NUM_PROT_BITS-1:0] awprot;
 
       // Write data channel
-      logic                          wvalid;
-      logic                          wready;
-      logic [$bits(conn.wdata)-1:0]  wdata;
-      logic [$bits(conn.wstrb)-1:0]  wstrb;
+      logic			wvalid;
+      logic			wready;
+      logic [NUM_DATA_BITS-1:0]	wdata;
+      logic [NUM_STRB_BITS-1:0]	wstrb;
 
       // Write response channel
-      logic                          bvalid;
-      logic                          bready;
-      logic [$bits(conn.bresp)-1:0]  bresp;
+      logic			bvalid;
+      logic			bready;
+      logic [NUM_RESP_BITS-1:0]	bresp;
 
       // Read address channel
-      logic                          arvalid;
-      logic                          arready;
-      logic [$bits(conn.araddr)-1:0] araddr;
-      logic [$bits(conn.arprot)-1:0] arprot;
+      logic			arvalid;
+      logic			arready;
+      logic [NUM_ADDR_BITS-1:0] araddr;
+      logic [NUM_PROT_BITS-1:0] arprot;
 
       // Read data channel
-      logic                          rvalid;
-      logic                          rready;
-      logic [$bits(conn.rdata)-1:0]  rdata;
-      logic [$bits(conn.rresp)-1:0]  rresp;
+      logic			rvalid;
+      logic			rready;
+      logic [NUM_DATA_BITS-1:0] rdata;
+      logic [NUM_RESP_BITS-1:0] rresp;
    } axi4_lite_beat_t;
 
 
+   // Write address channel
    typedef struct {
-      // Write address channel
-      logic                          awvalid;
-      // logic                          awready;
-      logic [$bits(conn.awaddr)-1:0] awaddr;
-      logic [$bits(conn.awprot)-1:0] awprot;
-
-      // Write data channel
-      logic                          wvalid;
-      // logic                          wready;
-      logic [$bits(conn.wdata)-1:0]  wdata;
-      logic [$bits(conn.wstrb)-1:0]  wstrb;
-
-      // Write response channel
-      // logic                          bvalid;
-      logic                          bready;
-      // logic [$bits(conn.bresp)-1:0]  bresp;
-   } axi4_lite_write_beat_t;
+      logic                     awvalid;
+      logic                     awready;
+      logic [NUM_ADDR_BITS-1:0] awaddr;
+      logic [NUM_PROT_BITS-1:0] awprot;
+   } axi4_lite_aw_beat_t;
 
 
+   // Write data channel
    typedef struct {
-      // Read address channel
-      logic                          arvalid;
-      // logic                          arready;
-      logic [$bits(conn.araddr)-1:0] araddr;
-      logic [$bits(conn.arprot)-1:0] arprot;
+      logic                     wvalid;
+      logic                     wready;
+      logic [NUM_DATA_BITS-1:0] wdata;
+      logic [NUM_STRB_BITS-1:0] wstrb;
+   } axi4_lite_w_beat_t;
 
-      // Read data channel
-      // logic                          rvalid;
-      logic                          rready;
-      // logic [$bits(conn.rdata)-1:0]  rdata;
-      // logic [$bits(conn.rresp)-1:0]  rresp;
-   } axi4_lite_read_beat_t;
 
+   // Write response channel
+   typedef struct {
+      logic                     bvalid;
+      logic                     bready;
+      logic [NUM_RESP_BITS-1:0] bresp;
+   } axi4_lite_b_beat_t;
+
+
+   // Read address channel
+   typedef struct {
+      logic                     arvalid;
+      logic                     arready;
+      logic [NUM_ADDR_BITS-1:0] araddr;
+      logic [NUM_PROT_BITS-1:0] arprot;
+   } axi4_lite_ar_beat_t;
+
+   // Read data channel
+   typedef struct {
+      logic                      rvalid;
+      logic                      rready;
+      logic [NUM_DATA_BITS-1:0]	 rdata;
+      logic [NUM_RRESP_BITS-1:0] rresp;
+   } axi4_lite_r_beat_t;
 
 
    typedef mailbox #(axi4_lite_beat_t) axi4_lite_inbox_t;
@@ -112,9 +175,9 @@ module axi4_lite_master_bfm(conn);
     * Write data transaction
     **************************************************************************/
    task write;
-      input  logic [$bits(conn.wdata)-1:0]  data;
-      input  logic [$bits(conn.awaddr)-1:0] addr;
-      output logic [$bits(conn.bresp)-1:0]  resp;
+      input  logic [NUM_DATA_BITS-1:0]  data;
+      input  logic [NUM_ADDR_BITS-1:0]	addr;
+      output logic [NUM_RESP_BITS-1:0]  resp;
       begin
 	 $timeformat(-9, 2, " ns", 20);
 	 $display("%t: m_axil - Write Data - Addr: %X, Data: %x", $time, addr, data);
@@ -138,8 +201,8 @@ module axi4_lite_master_bfm(conn);
     * Read data transaction
     **************************************************************************/
    task read;
-      input  logic [$bits(conn.awaddr)-1:0] addr;
-      output logic [$bits(conn.wdata)-1:0]  data;
+      input  logic [NUM_ADDR_BITS-1:0] addr;
+      output logic [NUM_DATA_BITS-1:0]  data;
       begin
 	 read_addr.put_simple_beat(addr);
 	 read_data.expect_beat(32'h0);
@@ -154,52 +217,52 @@ module axi4_lite_master_bfm(conn);
    ////////////////////////////////////////////////////////////////////////////
    ////////////////////////////////////////////////////////////////////////////
    // Write address channel
-   handshake_if #(.DATA_BITS($bits(conn.awaddr)+$bits(conn.awprot))) aw_conn(.clk(conn.aclk), .rst(conn.aresetn));
-   handshake_master #(.IFACE_NAME("m_axil_aw")) write_addr(aw_conn);
+   handshake_if #(.DATA_BITS($bits(axi4_lite_aw_beat_t)-2) aw_conn(.clk(conn.aclk), .rst(conn.aresetn));
+   handshake_master #(.IFACE_NAME($sformatf("m_axi4_lite_%s_aw", BFM_NAME))) write_addr(aw_conn);
 
    assign conn.awvalid  = aw_conn.valid;
    assign aw_conn.ready = conn.awready;
-   assign conn.awaddr   = aw_conn.data[($bits(conn.awaddr)-1):0];
-   assign conn.awprot   = aw_conn.data[($bits(conn.awprot)-1)+$bits(conn.awaddr):$bits(conn.awaddr)];
+   assign conn.awaddr   = aw_conn.data[AWADDR_OFFSET +: NUM_AWADDR_BITS];
+   assign conn.awprot   = aw_conn.data[AWPROT_OFFSET +: NUM_AWPROT_BITS];
 
 
    // Write data channel
-   handshake_if #(.DATA_BITS($bits(conn.wdata)+$bits(conn.wstrb))) w_conn(.clk(conn.aclk), .rst(conn.aresetn));
-   handshake_master #(.IFACE_NAME("m_axil_w")) write_data(w_conn);
+   handshake_if #(.DATA_BITS($bits(axi4_lite_w_beat_t)-2) w_conn(.clk(conn.aclk), .rst(conn.aresetn));
+   handshake_master #(.IFACE_NAME($sformatf("m_axi4_lite_%s_w", BFM_NAME))) write_data(w_conn);
 
    assign conn.wvalid  = w_conn.valid;
    assign w_conn.ready = conn.wready;
-   assign conn.wdata   = w_conn.data[($bits(conn.wdata)-1):0];
-   assign conn.wstrb   = w_conn.data[($bits(conn.wstrb)-1)+$bits(conn.wdata):$bits(conn.wdata)];
+   assign conn.wdata   = w_conn.data[WDATA_OFFSET +: NUM_WDATA_BITS];
+   assign conn.wstrb   = w_conn.data[WSTRB_OFFSET +: NUM_WSTRB_BITS];
 
 
    // Write response channel
-   handshake_if #(.DATA_BITS($bits(conn.bresp))) b_conn(.clk(conn.aclk), .rst(conn.aresetn));
-   handshake_slave #(.ALWAYS_READY(0), .IFACE_NAME("m_axil_bresp")) bresp(b_conn);
+   handshake_if #(.DATA_BITS($bits(axi4_lite_b_beat_t)-2)) b_conn(.clk(conn.aclk), .rst(conn.aresetn));
+   handshake_slave #(.ALWAYS_READY(0), .IFACE_NAME($sformatf("m_axi4_lite_%s_b", BFM_NAME))) bresp(b_conn);
 
    assign b_conn.valid = conn.bvalid;
    assign conn.bready  = b_conn.ready;
-   assign b_conn.data[($bits(conn.bresp)-1):0] = conn.bresp;
+   assign b_conn.data[BRESP_OFFSET +: NUM_BRESP_BITS] = conn.bresp;
 
 
    // Read address channel
-   handshake_if #(.DATA_BITS($bits(conn.araddr)+$bits(conn.arprot))) ar_conn(.clk(conn.aclk), .rst(conn.aresetn));
-   handshake_master #(.IFACE_NAME("m_axil_ar")) read_addr(ar_conn);
+   handshake_if #(.DATA_BITS($bits(axi4_lite_ar_beat_t)-2)) ar_conn(.clk(conn.aclk), .rst(conn.aresetn));
+   handshake_master #(.IFACE_NAME($sformatf("m_axi4_lite_%s_ar", BFM_NAME))) read_addr(ar_conn);
 
    assign conn.arvalid  = ar_conn.valid;
    assign ar_conn.ready = conn.arready;
-   assign conn.araddr   = ar_conn.data[($bits(conn.araddr)-1):0];
-   assign conn.arprot   = ar_conn.data[($bits(conn.arprot)-1)+$bits(conn.araddr):$bits(conn.araddr)];
+   assign conn.araddr   = ar_conn.data[ARADDR_OFFSET +: NUM_ARADDR_BITS];
+   assign conn.arprot   = ar_conn.data[ARPROT_OFFSET +: NUM_ARPROT_BITS];
 
 
    // Read data channel
-   handshake_if #(.DATA_BITS($bits(conn.rdata)+$bits(conn.rresp))) r_conn(.clk(conn.aclk), .rst(conn.aresetn));
-   handshake_slave #(.ALWAYS_READY(0), .IFACE_NAME("m_axil_r")) read_data(r_conn);
+   handshake_if #(.DATA_BITS($bits(axi4_lite_r_beat_t)-2)) r_conn(.clk(conn.aclk), .rst(conn.aresetn));
+   handshake_slave #(.ALWAYS_READY(0), .IFACE_NAME($sformatf("m_axi4_lite_%s_r", BFM_NAME))) read_data(r_conn);
 
    assign r_conn.valid = conn.rvalid;
    assign conn.rready  = r_conn.ready;
-   assign r_conn.data[($bits(conn.rdata)-1):0] = conn.rdata;
-   assign r_conn.data[($bits(conn.rresp)-1)+$bits(conn.rdata):$bits(conn.rdata)] = conn.rresp;
+   assign r_conn.data[RDATA_OFFSET +: NUM_RDATA_BITS] = conn.rdata;
+   assign r_conn.data[RRESP_OFFSET +: NUM_RRESP_BITS] = conn.rresp;
 
 
 endmodule // axi4_lite_master_bfm
--- src/rtl/axi4_lite_slave_bfm.sv
+++ src/rtl/axi4_lite_slave_bfm.sv
@@ -1,36 +1,136 @@
 module axi4_lite_slave_bfm(conn);
    axi4_lite_if conn;
 
+   ////////////////////////////////////////////////////////////////////////////
+   // Bit widths
+   ////////////////////////////////////////////////////////////////////////////
+   localparam NUM_ADDR_BITS   = conn.NUM_ADDR_BITS;
+   localparam NUM_DATA_BITS   = conn.NUM_DATA_BITS;
+   localparam NUM_STRB_BITS   = conn.NUM_STRB_BITS;
+   localparam NUM_RESP_BITS   = conn.NUM_RESP_BITS;
+   localparam NUM_PROT_BITS   = conn.NUM_PROT_BITS;
+
+   // Write address
+   localparam NUM_AWADDR_BITS = NUM_ADDR_BITS;
+   localparam NUM_AWPROT_BITS = NUM_PROT_BITS;
+
+   // Write data channel
+   localparam NUM_WDATA_BITS  = NUM_DATA_BITS;
+   localparam NUM_WSTRB_BITS  = NUM_STRB_BITS;
+
+   // Write response channel
+   localparam NUM_BRESP_BITS  = NUM_RESP_BITS;
+
+   // Read address channel
+   localparam NUM_ARADDR_BITS = NUM_ADDR_BITS;
+   localparam NUM_ARPROT_BITS = NUM_PROT_BITS;
+
+   // Read data channel
+   localparam NUM_RDATA_BITS  = NUM_DATA_BITS;
+   localparam NUM_RRESP_BITS  = NUM_RESP_BITS;
+
+
+
+   ////////////////////////////////////////////////////////////////////////////
+   // Offsets
+   ////////////////////////////////////////////////////////////////////////////
+   // Write address
+   localparam AWADDR_OFFSET = 0;
+   localparam AWPROT_OFFSET = AWADDR_OFFSET + NUM_ADDR_BITS;
+
+   // Write data
+   localparam WDATA_OFFSET  = 0;
+   localparam WSTRB_OFFSET  = WDATA_OFFSET + NUM_DATA_BITS;
+
+   // Write response
+   localparam BRESP_OFFSET  = 0;
+
+   // Read address
+   localparam ARADDR_OFFSET = 0;
+   localparam ARPROT_OFFSET = ARADDR_OFFSET + NUM_ADDR_BITS;
+
+   // Read data
+   localparam RDATA_OFFSET  = 0;
+   localparam RRESP_OFFSET  = RDATA_OFFSET + NUM_DATA_BITS;
+
+
+   ////////////////////////////////////////////////////////////////////////////
+   // Channel Structs
+   ////////////////////////////////////////////////////////////////////////////
    typedef struct {
       // Write address channel
-      logic                          awvalid;
-      logic                          awready;
-      logic [$bits(conn.awaddr)-1:0] awaddr;
-      logic [$bits(conn.awprot)-1:0] awprot;
+      logic                     awvalid;
+      logic                     awready;
+      logic [NUM_ADDR_BITS-1:0]	awaddr;
+      logic [NUM_PROT_BITS-1:0] awprot;
 
       // Write data channel
-      logic                          wvalid;
-      logic                          wready;
-      logic [$bits(conn.wdata)-1:0]  wdata;
-      logic [$bits(conn.wstrb)-1:0]  wstrb;
+      logic			wvalid;
+      logic			wready;
+      logic [NUM_DATA_BITS-1:0]	wdata;
+      logic [NUM_STRB_BITS-1:0]	wstrb;
 
       // Write response channel
-      logic                          bvalid;
-      logic                          bready;
-      logic [$bits(conn.bresp)-1:0]  bresp;
+      logic			bvalid;
+      logic			bready;
+      logic [NUM_RESP_BITS-1:0]	bresp;
 
       // Read address channel
-      logic                          arvalid;
-      logic                          arready;
-      logic [$bits(conn.araddr)-1:0] araddr;
-      logic [$bits(conn.arprot)-1:0] arprot;
+      logic			arvalid;
+      logic			arready;
+      logic [NUM_ADDR_BITS-1:0] araddr;
+      logic [NUM_PROT_BITS-1:0] arprot;
 
       // Read data channel
-      logic                          rvalid;
-      logic                          rready;
-      logic [$bits(conn.rdata)-1:0]  rdata;
-      logic [$bits(conn.rresp)-1:0]  rresp;
+      logic			rvalid;
+      logic			rready;
+      logic [NUM_DATA_BITS-1:0] rdata;
+      logic [NUM_RESP_BITS-1:0] rresp;
    } axi4_lite_beat_t;
+
+
+   // Write address channel
+   typedef struct {
+      logic                     awvalid;
+      logic                     awready;
+      logic [NUM_ADDR_BITS-1:0] awaddr;
+      logic [NUM_PROT_BITS-1:0] awprot;
+   } axi4_lite_aw_beat_t;
+
+
+   // Write data channel
+   typedef struct {
+      logic                     wvalid;
+      logic                     wready;
+      logic [NUM_DATA_BITS-1:0] wdata;
+      logic [NUM_STRB_BITS-1:0] wstrb;
+   } axi4_lite_w_beat_t;
+
+
+   // Write response channel
+   typedef struct {
+      logic                     bvalid;
+      logic                     bready;
+      logic [NUM_RESP_BITS-1:0] bresp;
+   } axi4_lite_b_beat_t;
+
+
+   // Read address channel
+   typedef struct {
+      logic                     arvalid;
+      logic                     arready;
+      logic [NUM_ADDR_BITS-1:0] araddr;
+      logic [NUM_PROT_BITS-1:0] arprot;
+   } axi4_lite_ar_beat_t;
+
+   // Read data channel
+   typedef struct {
+      logic                      rvalid;
+      logic                      rready;
+      logic [NUM_DATA_BITS-1:0]	 rdata;
+      logic [NUM_RRESP_BITS-1:0] rresp;
+   } axi4_lite_r_beat_t;
+
 
    typedef mailbox #(axi4_lite_beat_t) axi4_lite_inbox_t;
 
@@ -45,9 +145,9 @@ module axi4_lite_slave_bfm(conn);
     * Receive data from a master write
     **************************************************************************/
    task receive;
-      input  logic [$bits(conn.bresp)-1:0]  resp;
-      output logic [$bits(conn.wdata)-1:0]  data;
-      output logic [$bits(conn.awaddr)-1:0] addr;
+      input  logic [NUM_BRESP_BITS -1:0] resp;
+      output logic [NUM_WDATA_BITS -1:0] data;
+      output logic [NUM_AWADDR_BITS-1:0] addr;
       begin
 	 write_addr.get_beat(addr);
 	 write_data.get_beat(data);
@@ -60,8 +160,8 @@ module axi4_lite_slave_bfm(conn);
     * Respond to a master read
     **************************************************************************/
    task respond;
-      input  logic [$bits(conn.awaddr)-1:0] addr;
-      input  logic [$bits(conn.wdata)-1:0]  data;
+      input  logic [NUM_AWADDR_BITS-1:0] addr;
+      input  logic [NUM_WDATA_BITS-1:0]  data;
       begin
 	 read_addr.get_beat(addr);
 	 read_data.put_simple_beat(data);
@@ -75,51 +175,51 @@ module axi4_lite_slave_bfm(conn);
    ////////////////////////////////////////////////////////////////////////////
    ////////////////////////////////////////////////////////////////////////////
    // Write address channel
-   handshake_if #(.DATA_BITS($bits(conn.awaddr)+$bits(conn.awprot))) aw_conn(.clk(conn.aclk), .rst(conn.aresetn));
-   handshake_slave #(.ALWAYS_READY(0), .IFACE_NAME("s_axil_aw")) write_addr(aw_conn);
+   handshake_if #(.DATA_BITS($bits(axi4_lite_aw_beat_t)-2)) aw_conn(.clk(conn.aclk), .rst(conn.aresetn));
+   handshake_slave #(.ALWAYS_READY(0), .IFACE_NAME($sformatf("s_axi4_lite_%s_aw", BFM_NAME))) write_addr(aw_conn);
 
    assign aw_conn.valid = conn.awvalid;
    assign conn.awready  = aw_conn.ready;
-   assign aw_conn.data[($bits(conn.awaddr)-1):0] = conn.awaddr;
-   assign aw_conn.data[($bits(conn.awprot)-1)+$bits(conn.awaddr):$bits(conn.awaddr)] = conn.awprot;
+   assign aw_conn.data[AWADDR_OFFSET +: NUM_AWADDR_BITS] = conn.awaddr;
+   assign aw_conn.data[AWPROT_OFFSET +: NUM_AWPROT_BITS] = conn.awprot;
 
 
    // Write data channel
-   handshake_if #(.DATA_BITS($bits(conn.wdata)+$bits(conn.wstrb))) w_conn(.clk(conn.aclk), .rst(conn.aresetn));
-   handshake_slave #(.ALWAYS_READY(0), .IFACE_NAME("s_axil_w")) write_data(w_conn);
+   handshake_if #(.DATA_BITS($bits(axi4_lite_w_beat_t)-2)) w_conn(.clk(conn.aclk), .rst(conn.aresetn));
+   handshake_slave #(.ALWAYS_READY(0), .IFACE_NAME($sformatf("s_axi4_lite_%s_w", BFM_NAME))) write_data(w_conn);
 
    assign w_conn.valid = conn.wvalid;
    assign conn.wready  = w_conn.ready;
-   assign w_conn.data[($bits(conn.wdata)-1):0] = conn.wdata;
-   assign w_conn.data[($bits(conn.wstrb)-1)+$bits(conn.wdata):$bits(conn.wdata)] = conn.wstrb;
+   assign w_conn.data[WDATA_OFFSET +: NUM_WDATA_BITS] = conn.wdata;
+   assign w_conn.data[WSTRB_OFFSET +: NUM_WSTRB_BITS] = conn.wstrb;
 
 
    // Write response channel
-   handshake_if #(.DATA_BITS($bits(conn.bresp))) b_conn(.clk(conn.aclk), .rst(conn.aresetn));
-   handshake_master #(.IFACE_NAME("s_axil_bresp")) bresp(b_conn);
+   handshake_if #(.DATA_BITS($bits(axi4_lite_b_beat_t)-2)) b_conn(.clk(conn.aclk), .rst(conn.aresetn));
+   handshake_master #(.IFACE_NAME($sformatf("s_axi4_lite_%s_b", BFM_NAME))) bresp(b_conn);
 
    assign conn.bvalid  = b_conn.valid;
    assign b_conn.ready = conn.bready;
-   assign conn.bresp   = b_conn.data[($bits(conn.bresp)-1):0];
+   assign conn.bresp   = b_conn.data[BRESP_OFFSET +: NUM_BRESP_BITS];
 
 
    // Read address channel
-   handshake_if #(.DATA_BITS($bits(conn.awaddr)+$bits(conn.arprot))) ar_conn(.clk(conn.aclk), .rst(conn.aresetn));
-   handshake_slave #(.ALWAYS_READY(0), .IFACE_NAME("s_axil_ar")) read_addr(ar_conn);
+   handshake_if #(.DATA_BITS($bits(axi4_lite_ar_beat_t)-2)) ar_conn(.clk(conn.aclk), .rst(conn.aresetn));
+   handshake_slave #(.ALWAYS_READY(0), .IFACE_NAME($sformatf("s_axi4_lite_%s_ar", BFM_NAME))) read_addr(ar_conn);
 
    assign ar_conn.valid = conn.arvalid;
    assign conn.arready  = ar_conn.ready;
-   assign ar_conn.data[($bits(conn.araddr)-1):0] = conn.araddr;
-   assign ar_conn.data[($bits(conn.arprot)-1)+$bits(conn.araddr):$bits(conn.araddr)] = conn.arprot;
+   assign ar_conn.data[ARADDR_OFFSET +: NUM_ARADDR_BITS] = conn.araddr;
+   assign ar_conn.data[ARPROT_OFFSET +: NUM_ARPROT_BITS] = conn.arprot;
 
 
    // Read data channel
-   handshake_if #(.DATA_BITS($bits(conn.rdata)+$bits(conn.rresp))) r_conn(.clk(conn.aclk), .rst(conn.aresetn));
-   handshake_master #(.IFACE_NAME("s_axil_r")) read_data(r_conn);
+   handshake_if #(.DATA_BITS($bits(axi4_lite_r_beat_t)-2)) r_conn(.clk(conn.aclk), .rst(conn.aresetn));
+   handshake_master #(.IFACE_NAME($sformatf("s_axi4_lite_%s_r", BFM_NAME))) read_data(r_conn);
 
    assign conn.rvalid  = r_conn.valid;
    assign r_conn.ready = conn.rready;
-   assign conn.rdata   = r_conn.data[($bits(conn.rdata)-1):0];
-   assign conn.rresp   = r_conn.data[($bits(conn.rresp)-1)+$bits(conn.rdata):$bits(conn.rdata)];
+   assign conn.rdata   = r_conn.data[RDATA_OFFSET +: NUM_RDATA_BITS];
+   assign conn.rresp   = r_conn.data[RRESP_OFFSET +: NUM_RRESP_BITS];
 
 endmodule // axi4_lite_slave_bfm
